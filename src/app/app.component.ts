1299ef2c2eeb2c94c8af82ead6ef9d4656d3da71
unknown <mrahmeddorhhmi@gmail.com>
2019-12-08 22:35:16 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'workshop5';
+  users = [];
+  message = "";
+  status = 0;
+  addUser($event){
+    this.status  = 0;
+      if($event.name == "" || $event.prenom=="")
+      {
+        this.message = "les champs sont obligatoires"
+        this.status = -1
+      }
+      else if(this.users.length == 5)
+      {
+        this.message="max user atteint";
+        this.status= -1
+      }
+      else
+      {
+        console.log($event)
+        let user = {nom:$event.name,prenom:$event.prenom};
+        this.users.push(user);
+        this.message="ok";
+        this.status=1
+      }
+      console.log(this.users)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlerteComponent } from './alerte/alerte.component';
+import { UsersComponent } from './users/users.component';
+import { FormulaireComponent } from './formulaire/formulaire.component';
+import { FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlerteComponent,
+    UsersComponent,
+    FormulaireComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
